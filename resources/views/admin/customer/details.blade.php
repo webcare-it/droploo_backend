4cd9be9b4e20357e19308b5909964d925e34a959
Md Arman Ali <mdarmanali070@gmail.com>
2025-07-06 15:14:45 +0600

    Update details.blade.php

--- resources/views/admin/customer/details.blade.php
+++ resources/views/admin/customer/details.blade.php
@@ -54,7 +54,7 @@
                                             <label for="store">
                                                 Store <span style="color: red;">*</span>
                                             </label>
-                                            <input type="text" name="store" class="form-control" value="Nittoz.com"
+                                            <input type="text" name="store" class="form-control" value="droploo.com"
                                                 readonly>
                                         </div>
                                     </div>
