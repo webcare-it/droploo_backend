da1c11f4b6d12042d6051642f0ae6d99bdf3035f
Md Arman Ali <mdarmanali070@gmail.com>
2025-08-26 16:26:35 +0600

    Update details.blade.php

--- resources/views/admin/customer/details.blade.php
+++ resources/views/admin/customer/details.blade.php
@@ -153,7 +153,7 @@
                                             </label>
                                             <textarea class="form-control" rows="4" name="pathao_special_note">{{ $order->pathao_special_note ?? 'No Notes' }}</textarea>
                                             @if ($order->order_type == 'Dropshipping')
-                                                <textarea class="form-control" style="display: none" rows="4" name="notes">
+                                                <textarea class="form-control" rows="4" name="notes">
                                             প্রথমে এই সাব মার্চেন্ট
                                             নাম্বারে কল করবেন
                                             {{ $order->dropshipper->phone }} যদি এই
