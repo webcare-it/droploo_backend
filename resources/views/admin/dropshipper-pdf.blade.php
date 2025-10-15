9f417e2cb073595b9deb212ff51f21760a5d9806
Md Arman Ali <ceowebcareit@gmail.com>
2025-10-15 19:16:51 +0600

    Update dropshipper-pdf.blade.php

--- resources/views/admin/dropshipper-pdf.blade.php
+++ resources/views/admin/dropshipper-pdf.blade.php
@@ -76,11 +76,11 @@
             font-weight: 700;
             border-bottom: 1px solid #f48fb1;
             margin-bottom: 3px;
-            font-size: 13px;
+            font-size: 12px;
         }
 
         .info-box p {
-            font-size: 13px;
+            font-size: 11px;
             font-weight: 600;
             margin: 0;
             line-height: 1.3;
@@ -103,13 +103,13 @@
 
         .invoice-table td {
             border: 1px solid #ddd;
-            font-size: 13px;
+            font-size: 11px;
             padding: 3px 5px;
         }
 
         .totals {
             width: 100%;
-            font-size: 13px;
+            font-size: 11px;
         }
 
         .totals td {
@@ -119,7 +119,7 @@
         }
 
         .totals tr:last-child td {
-            font-size: 13px;
+            font-size: 12px;
             color: #c2185b;
             font-weight: 700;
         }
