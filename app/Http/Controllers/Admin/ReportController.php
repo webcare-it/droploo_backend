f144692f1bf0da12af798181b2c4beb2213132cc
Md Arman Ali <ceowebcareit@gmail.com>
2025-12-07 15:09:16 +0600

    Update ReportController.php

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -77,7 +77,7 @@ class ReportController extends Controller
         $selectedColumns = $request->columns ?? [
             'ItemType(*)',
             'StoreName(*)',
-            'MerchantOrderId',
+            'OrderId',
             'RecipientName(*)',
             'RecipientPhone(*)',
             'RecipientCity(*)',
